c80eabc8790dfdbc6fe1ced0d04da32f6fc0aa43
plvhx <gandung@slackerc0de.us>
2017-05-27 05:11:54 +0700

    ..

--- src/InvokerInterface.php
+++ src/InvokerInterface.php
@@ -4,7 +4,17 @@ namespace Experiments\DependencyInjection;
 
 interface InvokerInterface
 {
+	/**
+	 * Executing function or \Closure with supplied arguments.
+	 *
+	 * @return mixed
+	 */
 	public function invoke();
 
+	/**
+	 * Executing function or \Closure with packed arguments.
+	 *
+	 * @return mixed
+	 */
 	public function invokeArgs($args = []);
 }
