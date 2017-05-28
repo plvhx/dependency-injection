262091856aadc1c993682d9cf23f74bd86620d9c
plvhx <gandung@slackerc0de.us>
2017-05-28 17:39:56 +0700

    Adding more tests.

--- tests/ContainerTest.php
+++ tests/ContainerTest.php
@@ -8,7 +8,10 @@ namespace DependencyInjection\Tests;
 
 use DependencyInjection\Container;
 use DependencyInjection\Tests\Fixtures\Base;
+use DependencyInjection\Tests\Fixtures\BaseInterface;
 use DependencyInjection\Tests\Fixtures\Foo;
+use DependencyInjection\Tests\Fixtures\FooWithInterface;
+use DependencyInjection\Tests\Fixtures\Bar;
 
 class ContainerTest extends \PHPUnit_Framework_TestCase
 {
@@ -68,5 +71,97 @@ class ContainerTest extends \PHPUnit_Framework_TestCase
 
 		$this->assertFalse($container->isAbstractExists(Foo::class));
 		$this->assertInstanceOf('DependencyInjection\\Tests\\Fixtures\\Foo', $foo);
+	}
+
+	public function testCanDoConditionalBindingOnBaseClass()
+	{
+		$container = new Container();
+
+		$container->bindIf(Base::class, function($container) {
+			return $container->make(Base::class);
+		});
+
+		$this->assertTrue($container->isAbstractExists(Base::class));
+
+		$base = $container->make(Base::class);
+
+		$this->assertFalse($container->isAbstractExists(Base::class));
+		$this->assertInstanceOf('DependencyInjection\\Tests\\Fixtures\\Base', $base);
+	}
+
+	public function testCanDoConditionalBindingOnInheritedClass()
+	{
+		$container = new Container();
+
+		$container->bindIf(Foo::class, function($container) {
+			return $container->make(Base::class);
+		});
+
+		$this->assertTrue($container->isAbstractExists(Foo::class));
+
+		$foo = $container->make(Foo::class);
+
+		$this->assertFalse($container->isAbstractExists(Foo::class));
+		$this->assertInstanceOf('DependencyInjection\\Tests\\Fixtures\\Foo', $foo);
+	}
+
+	public function testCanBindingConcreteClosureToAbstract()
+	{
+		$container = new Container();
+
+		$container->bind(BaseInterface::class, function($container) {
+			return new Base();
+		});
+
+		$this->assertTrue($container->isAbstractExists(BaseInterface::class));
+
+		$foo = $container->make(FooWithInterface::class);
+
+		$this->assertFalse($container->isAbstractExists(BaseInterface::class));
+		$this->assertInstanceOf('DependencyInjection\\Tests\\Fixtures\\FooWithInterface', $foo);
+	}
+
+	public function testCanBindingConcreteToAbstract()
+	{
+		$container = new Container();
+
+		$container->bind(BaseInterface::class, Base::class);
+
+		$this->assertTrue($container->isAbstractExists(BaseInterface::class));
+
+		$foo = $container->make(FooWithInterface::class);
+
+		$this->assertFalse($container->isAbstractExists(BaseInterface::class));
+		$this->assertInstanceOf('DependencyInjection\\Tests\\Fixtures\\FooWithInterface', $foo);
+	}
+
+	public function testCanBindingConcreteClosureToAbstractCircularly()
+	{
+		$container = new Container();
+
+		$container->bind(BaseInterface::class, function($container) {
+			return $container->make(Base::class);
+		});
+
+		$this->assertTrue($container->isAbstractExists(BaseInterface::class));
+
+		$bar = $container->make(Bar::class);
+
+		$this->assertFalse($container->isAbstractExists(BaseInterface::class));
+		$this->assertInstanceOf('DependencyInjection\\Tests\\Fixtures\\Bar', $bar);
+	}
+
+	public function testCanBindingConcreteToAbstractCircularly()
+	{
+		$container = new Container();
+
+		$container->bind(BaseInterface::class, Base::class);
+
+		$this->assertTrue($container->isAbstractExists(BaseInterface::class));
+
+		$bar = $container->make(Bar::class);
+
+		$this->assertFalse($container->isAbstractExists(BaseInterface::class));
+		$this->assertInstanceOf('DependencyInjection\\Tests\\Fixtures\\Bar', $bar);
 	}
 }
