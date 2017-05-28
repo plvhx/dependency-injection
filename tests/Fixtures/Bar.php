8224a42aa2537545e613688d76f0b3bf1d55ffd4
plvhx <gandung@slackerc0de.us>
2017-05-28 17:40:35 +0700

    Adding more fixtures.

---
+++ tests/Fixtures/Bar.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace DependencyInjection\Tests\Fixtures;
+
+class Bar
+{
+	/**
+	 * @var FooWithInterface
+	 */
+	private $foo;
+
+	public function __construct(FooWithInterface $foo)
+	{
+		$this->foo = $foo;
+	}
+}
---
+++ tests/Fixtures/FooWithInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace DependencyInjection\Tests\Fixtures;
+
+class FooWithInterface
+{
+	/**
+	 * @var BaseInterface
+	 */
+	private $base;
+
+	public function __construct(BaseInterface $base)
+	{
+		$this->base = $base;
+	}
+}
